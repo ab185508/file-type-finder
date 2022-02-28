5c334fbab857d8b70151ce9330473a9e5e0aebc5
Alex Breazu <ab185508@ncr.com>
2022-02-28 15:39:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ def set_action_output(name: str, value: str):
 def main():
     path = os.environ["INPUT_PATH"]
     extension = os.environ["INPUT_TYPE"]
-    
-    paths = ''
+
+    paths = []
+    names = []
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(f'{extension}'):
                 paths = paths + root + '/' + str(file) + ' '
-                names = os.path.splitext(file)[0]
+                names = names + os.path.splitext(file)[0]
 
     set_action_output('paths', paths)
     set_action_output('names', names)
