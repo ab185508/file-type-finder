5c10bebffe811b06fbba02a10f1e9b183f2d6124
Alex Breazu <ab185508@ncr.com>
2022-03-02 14:43:32 -0500

    Implemented more features and use case test

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ def set_action_output(name: str, value: str):
 def main():
     path = os.environ["INPUT_PATH"]
     extension = os.environ["INPUT_TYPE"]
+    input = os.environ["INPUT_FILEINPUT"]
+    accessfile = os.environ["INPUT_FILE"]
 
     paths = []
     names = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
+    if input == "true":
+        with open(accessfile, 'r') as f:
+            data = f.read()
+        for file in data:
             if file.endswith(f'{extension}'):
                 path_str = root + '/' + str(file) + ' '
                 paths.append(path_str)
                 names.append(os.path.splitext(file)[0])
+    else:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(f'{extension}'):
+                    path_str = root + '/' + str(file) + ' '
+                    paths.append(path_str)
+                    names.append(os.path.splitext(file)[0])
 
     set_action_output('paths', paths)
     set_action_output('names', names)
